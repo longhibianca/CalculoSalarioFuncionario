a7a1162ec55d69ab332d3a1b38a1a8da0fe046ce
Bianca Longhi <longhisbianca@gmail.com>
2021-11-06 15:03:29 -0300

    Criando o projeto para calcular o salário do funcionário com base em contratos

---
+++ CalculoSalarioFuncionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoSalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
