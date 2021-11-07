ddeb75c76846cdac1ce749773a4138862f7020fd
Bianca Longhi <longhisbianca@gmail.com>
2021-11-07 11:22:56 -0300

    Criando a operação para calcular o salário

--- CalculoSalarioFuncionario/Entities/Worker.cs
+++ CalculoSalarioFuncionario/Entities/Worker.cs
@@ -36,6 +36,17 @@ namespace CalculoSalarioFuncionario.Entities
             Contracts.Remove(contract);
         }
 
-
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum = sum + contract.TotalValue();
+                }
+            }
+            return sum;
+        }
     }
 }
