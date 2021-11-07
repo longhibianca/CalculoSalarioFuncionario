62dc8bbec3ccd5c7e3ca1a999f2599cccafcf2f2
Bianca Longhi <longhisbianca@gmail.com>
2021-11-07 11:11:10 -0300

    Criando as classes e atributos do projeto

---
+++ CalculoSalarioFuncionario/Entities/Worker.cs
@@ -0,0 +1,19 @@
+﻿using CalculoSalarioFuncionario.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoSalarioFuncionario.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+    
+
+
+    }
+}
