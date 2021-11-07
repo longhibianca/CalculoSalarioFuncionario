3649b250c1ac5cbb61a58892118e169d40b06ea5
Bianca Longhi <longhisbianca@gmail.com>
2021-11-07 11:15:48 -0300

    Criando os métodos de adicionar e remover contrato

--- CalculoSalarioFuncionario/Entities/Worker.cs
+++ CalculoSalarioFuncionario/Entities/Worker.cs
@@ -13,6 +13,28 @@ namespace CalculoSalarioFuncionario.Entities
         public Departament Departament { get; set; }
         public List<HourContract> Contracts { get; set; } = new List<HourContract>();
     
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract (HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
 
 
     }
